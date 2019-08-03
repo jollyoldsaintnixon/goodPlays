1859c47d2bfa64224c8e125c46f3d2bba607c3c2
jollyoldsaintnixon <appapplication@protonmail.com>
2019-08-03 15:41:27 -0400

    Prepare for Heroku push

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,32 +8,33 @@
 # require 'mechanize'
 require 'open-uri'
 
-table = CSV.read("#{Rails.root}/lib/assets/games-features.csv", headers: true)
 
 Game.destroy_all
 
-array_of_hashes = []
-table.each do |row|
-  temp_hash = {}
-  temp_hash[:title] = row[3]
-  temp_hash[:description] = row[64]
-  temp_hash[:release_date] = row[4]
-  temp_hash[:price] = row[58]
-  temp_hash[:image_url] = row[67]
-  array_of_hashes << temp_hash
-end
+# table = CSV.read("#{Rails.root}/lib/assets/games-features.csv", headers: true)
 
-files = 0
+# array_of_hashes = []
+# table.each do |row|
+#   temp_hash = {}
+#   temp_hash[:title] = row[3]
+#   temp_hash[:description] = row[64]
+#   temp_hash[:release_date] = row[4]
+#   temp_hash[:price] = row[58]
+#   temp_hash[:image_url] = row[67]
+#   array_of_hashes << temp_hash
+# end
+
+# files = 0
 
 
 
-File.open("#{Rails.root}/lib/assets/scraped.txt", 'w') do |file|
-  array_of_hashes.each do |el|
-    files += 1
-    break if files > 100 
-    file.puts(el.to_json)
-  end
-end
+# File.open("#{Rails.root}/lib/assets/scraped.txt", 'w') do |file|
+#   array_of_hashes.each do |el|
+#     files += 1
+#     break if files > 100 
+#     file.puts(el.to_json)
+#   end
+# end
 
 File.open("#{Rails.root}/lib/assets/scraped.txt", 'r') do |file|
   file.read.each_line do |line|

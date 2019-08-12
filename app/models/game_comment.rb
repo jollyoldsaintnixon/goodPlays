bc7b7be79e2b73afa7ef7f0d59437febc12fdff0
jOSN <appapplication@protonmail.com>
2019-08-12 15:42:32 -0400

    Modify GameComment validations

--- app/models/game_comment.rb
+++ app/models/game_comment.rb
@@ -1,14 +1,20 @@
 class GameComment < ApplicationRecord
     validates :title, :author_id, :game_id, presence: true
 
-    has_many :games
+    belongs_to :game
 
     belongs_to :author,
     class_name: :User,
-    foreign_key: :user_id,
-    primary_key: :id,
+    foreign_key: :author_id,
+    primary_key: :id
 
     belongs_to :parent,
+    class_name: :GameComment,
+    foreign_key: :parent_id,
+    primary_key: :id,
+    optional: true
+
+    has_many :children,
     class_name: :GameComment,
     foreign_key: :parent_id,
     primary_key: :id

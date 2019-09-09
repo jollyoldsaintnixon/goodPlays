bcb32d231bf1f48e13828c9b4b129f434bf129b0
jOSN <appapplication@protonmail.com>
2019-09-08 20:42:15 -0400

    Modify email to include clickable link and attachments

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-    default from: 'frgioff@gmail.com'
+    default from: 'no-reply@mail.com'
 
     def welcome_email(user)
         @user = user
@@ -7,7 +7,7 @@ class UserMailer < ApplicationMailer
         # all three of these file paths work. Sweet
         # attachments['relative.jpg'] = File.read('app/assets/images/capybara-swiming.jpg')
         # attachments['full.jpg'] = File.read('/Users/lylecrocodyle/Desktop/TLM/a:A/goodPlays/app/assets/images/capybara-swiming.jpg')
-        # attachments['rails_root.jpg'] = File.read("#{Rails.root}/app/assets/images/capybara-swiming.jpg")
-        mail(to: @user.email, subject: "Welcome")
+        attachments['rails_root.jpg'] = File.read("#{Rails.root}/app/assets/images/capybara-swiming.jpg")
+        mail(to: @user.email, subject: "Welcome to goodPlays", cc: 'tlmcmerty3@gmail.com')
     end
 end

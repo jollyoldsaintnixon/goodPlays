bbea401365c486253639e7a3997325d42e6b0fd3
jOSN <appapplication@protonmail.com>
2019-09-20 10:09:55 -0400

    Remove logger

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,10 +1,10 @@
 // Gems
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
-import logger from 'redux-logger'
+// import logger from 'redux-logger'
 // Components
 import RootReducer from '../reducers/root_reducer'
 
 export const configureStore = (preloadedState = {}) => {
-  return createStore(RootReducer, preloadedState, applyMiddleware(thunk, logger))
+  return createStore(RootReducer, preloadedState, applyMiddleware(thunk))
 } 
